4b4a351d7b833c74d06eb61274c549643e50b56c
Akramul-Haque-Rony <hsrony04@gmail.com>
2021-04-17 21:34:17 +0600

    Update HomeController.php
    comment out

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,21 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
@@ -53,11 +43,6 @@ class HomeController extends Controller
     public function payments()
     {
         return view('payments');
-    }
-
-    public function typography()
-    {
-        return view('typography');
     }
 
     public function duplicate()
